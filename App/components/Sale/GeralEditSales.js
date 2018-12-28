e8b948635806b897c9b805238da7a293e95e9029
Weslei Ferreira <desenvolvimento2@interativasistemas.com>
2018-12-28 18:01:03 -0200

    Adicionado tela de Gerais do Pedido

--- App/components/Sale/GeralEditSales.js
+++ App/components/Sale/GeralEditSales.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardItem, Body, Text, Item, Label, Button, Container } from 'native-base';
-import { Stylesheet } from 'react-native';
+import { Card, CardItem, Body, Text, Item, 
+        Label, Button, Container,Picker, Content, DatePicker, Textarea } from 'native-base';
+import { StyleSheet } from 'react-native';
 
 class GeralEditSales extends Component{
     render(){
@@ -9,26 +10,70 @@ class GeralEditSales extends Component{
                <Card>
                     <CardItem>
                         <Body>
+                            <DatePicker 
+                                locale="pr-BR"
+                                modalTransparent={false}
+                                animationType={"fade"}
+                                androidMode={"default"}
+                                placeHolderText="Data do orçamento"
+                                placeHolderTextStyle={{ color: "#d3d3d3" }}
+                            />   
                             <Button style={{ width:'100%', marginBottom:10 }}>
                                 <Text>Selecione um cliente</Text>
                             </Button>
-                            <Label>Cliente: </Label>
-                            <Label>CPF/CNPJ:</Label>
-                            <Label>Cidade/UF:</Label>
-                            <Label>Fone:</Label>
+                            <Label style ={ styles.text }>Cliente: </Label>
+                            <Label style ={ styles.text }>CPF/CNPJ:</Label>
+                            <Label style ={ styles.text }>Cidade/UF:</Label>
+                            <Label style ={ styles.text }>Fone:</Label>
+                            
                         </Body>
                     </CardItem>
                 </Card> 
                 <Card>
                     <CardItem>
-                        <Body>
-                            <Label>Cliente: </Label>
-                            <Label>CPF/CNPJ:</Label>
-                            <Label>Cidade/UF:</Label>
-                            <Label>Fone:</Label>
+                        <Body style={ styles.container }>                   
+                            <Content>
+                                <Label style={ styles.label }>Forma de Cobrança</Label>    
+                                <Picker
+                                    note
+                                    mode="dropdown"
+                                    style={{width: undefined} }                      
+                                    >
+                                        <Picker.Item label="Selecione" value="-1"/>
+                                        <Picker.Item label="Dinheiro" value="key0" />
+                                        <Picker.Item label="Cartão" value="key1" />
+                                        <Picker.Item label="Boleto" value="key2" />
+                                        <Picker.Item label="Cheque" value="key3" />
+                                </Picker>
+                            </Content>
+                            
+                            <Content >
+                                <Label style={ styles.label }>Condição Pagamento</Label>    
+                                <Picker
+                                note
+                                mode="dropdown"
+                                style={{width: undefined} }                      
+                                >
+                                    <Picker.Item label=" A vista" value="-1"/>
+                                    <Picker.Item label="30 + 60" value="key0" />
+                                    <Picker.Item label="90 + 120" value="key1" />
+                                </Picker>
+                            </Content>             
+                    
                         </Body>
                     </CardItem>
                 </Card> 
+
+                <Card>
+                    <CardItem>
+                        <Body style={ styles.container }>   
+                        <Content>
+                            <Label style={ styles.label }>Observações</Label>
+                            <Textarea rowSpan={2} bordered placeholder="Observações " />
+                        </Content>
+                        </Body>
+                    </CardItem>
+                </Card>
             </Container>
             
             
@@ -38,7 +83,20 @@ class GeralEditSales extends Component{
 
 const styles = StyleSheet.create({
     text:{
-        fontSize: 10
+        fontSize: 15
+    },
+    container:{
+        flexDirection: 'row',
+        marginBottom:0,
+        top:0 
+
+    },
+    select:{
+        flex: 1
+    },
+    label:{
+        fontSize:12,
+        fontWeight:'bold'
     }
 })
 
