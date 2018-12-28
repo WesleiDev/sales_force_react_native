67677164c47af5617d01ab15918ace46076293a8
Weslei Ferreira <desenvolvimento2@interativasistemas.com>
2018-12-28 16:42:59 -0200

    Melhorias

--- App/components/Sale/GeralEditSales.js
+++ App/components/Sale/GeralEditSales.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import { Card, CardItem, Body, Text } from 'native-base';
+import { Card, CardItem, Body, Text, Item, Label, Button, Container } from 'native-base';
+import { Stylesheet } from 'react-native';
 
 class GeralEditSales extends Component{
     render(){
         return(
-            <Card>
-            <CardItem header>
-                <Text>Informações Gerais</Text>
-            </CardItem>
-            <CardItem>
-            <Body>
-                <Text>
-                
-                </Text>
-            </Body>
-            </CardItem>
-            <CardItem footer>
-                <Text>Total: R$200,00</Text>
-            </CardItem>
-        </Card>
+            <Container>
+               <Card>
+                    <CardItem>
+                        <Body>
+                            <Button style={{ width:'100%', marginBottom:10 }}>
+                                <Text>Selecione um cliente</Text>
+                            </Button>
+                            <Label>Cliente: </Label>
+                            <Label>CPF/CNPJ:</Label>
+                            <Label>Cidade/UF:</Label>
+                            <Label>Fone:</Label>
+                        </Body>
+                    </CardItem>
+                </Card> 
+                <Card>
+                    <CardItem>
+                        <Body>
+                            <Label>Cliente: </Label>
+                            <Label>CPF/CNPJ:</Label>
+                            <Label>Cidade/UF:</Label>
+                            <Label>Fone:</Label>
+                        </Body>
+                    </CardItem>
+                </Card> 
+            </Container>
+            
+            
         )
     }
 }
+
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 10
+    }
+})
 
 export default GeralEditSales;
